8eaea4a1ed0e6396fe38c55156f21ebcc91b6c4d
Serendipity_advocator <jason24988114@gapp.nthu.edu.tw>
2019-11-10 02:56:44 +0800

    point class, shortest distance, battery check

--- robot.cpp
+++ robot.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 #include <string>
 
@@ -7,12 +8,65 @@ using namespace std;
 ifstream input("robot.data",ios::in);
 ofstream output("robot.final");
 
+class point
+{
+private:
+  int X = -1;
+  int Y = -1;
+  char shape;
+  int Dist_to_R = -1;
+  point* right;
+  point* left;
+  point* up;
+  point* down;
+public:
+  void set_X(int hor_index)
+  {
+    X = hor_index;
+  }
+  void set_Y(int ver_index)
+  {
+    Y = ver_index;
+  }
+  int get_X()
+  {
+    return X;
+  }
+  int get_Y()
+  {
+    return Y;
+  }
+  void set_shape(char ch)
+  {
+    shape = ch;
+  }
+  char get_shape()
+  {
+    return shape;
+  }
+  int Distance_counting(int x, int y)
+  {
+    return (abs(X-x)+abs(Y-y));
+  }
+  void set_D_to_R(int dist)
+  {
+    Dist_to_R = dist;
+  }
+  int get_D_to_R()
+  {
+    return Dist_to_R;
+  }
+};
+
+
 class clean_area
 {
+friend class point;
 private:
     int ver_vertex = 0;
     int hor_vertex = 0;
-    char** area;
+    int battery = 0;
+    point** area;
     int R_x = 0;
     int R_y = 0;
 public:
@@ -32,14 +86,22 @@ public:
     {
       return hor_vertex;
     }
+    void set_battery(int life_span)
+    {
+      battery = life_span;
+    }
+    int get_battery()
+    {
+      return battery;
+    }
     void print_clean_area()
     {
       for(int i=0; i<ver_vertex; i++)
       {
         for(int j=0; j<hor_vertex; j++)
         {
-          cout<<area[i][j];
-          //output<<area[i][j];
+          cout<<area[i][j].get_shape();
+          //output<<area[i][j].shape;
         }
         cout<<endl;
         //output<<endl;
@@ -48,88 +110,64 @@ public:
     bool edge_detection()
     {
       cout<<"Operating edge detection!"<<endl;
-      cout<<"detect the hor_vertexizontal edges."<<endl;
+      cout<<"detect the horizontal edges."<<endl;
       for(int i=0; i<ver_vertex; i++)
       {
-        cout<<"area["<<i<<"]["<<0<<"]"<<" = "<<area[i][0]<<endl;
-        if(area[i][0] == '0'){return 0;}
-        cout<<"area["<<i<<"]["<<hor_vertex-1<<"]"<<" = "<<area[i][hor_vertex-1]<<endl;
-        if(area[i][hor_vertex-1] == '0'){return 0;}
+        cout<<"area["<<i<<"]["<<0<<"]"<<" = "<<area[i][0].get_shape()<<endl;
+        if(area[i][0].get_shape() == '0'){return 0;}
+        cout<<"area["<<i<<"]["<<hor_vertex-1<<"]"<<" = "<<area[i][hor_vertex-1].get_shape()<<endl;
+        if(area[i][hor_vertex-1].get_shape() == '0'){return 0;}
       }
-      cout<<"detect the ver_vertextical edges."<<endl;
+      cout<<"detect the vertical edges."<<endl;
       for(int j=1; j<hor_vertex-1;j++)
       {
-        cout<<"area["<<0<<"]["<<j<<"]"<<" = "<<area[0][j]<<endl;
-        if(area[0][j] == '0')
+        cout<<"area["<<0<<"]["<<j<<"]"<<" = "<<area[0][j].get_shape()<<endl;
+        if(area[0][j].get_shape() == '0')
         {
-          cout<<"illegal!"<<endl;
+          cout<<"Illegal!"<<endl;
           return 0;
         }
-        cout<<"area["<<ver_vertex-1<<"]["<<j<<"]"<<" = "<<area[ver_vertex-1][j]<<endl;
-        if(area[ver_vertex-1][j] == '0'){return 0;}
+        cout<<"area["<<ver_vertex-1<<"]["<<j<<"]"<<" = "<<area[ver_vertex-1][j].get_shape()<<endl;
+        if(area[ver_vertex-1][j].get_shape() == '0'){return 0;}
       }
       cout<<"Finished!"<<endl;
       return 1;
      }
-    bool charging_place_detection()
-    {
-      int count =0;
-      for(int i=0; i<ver_vertex; i++)
-      {
-        for(int j=0; j<hor_vertex; j++)
-        {
-          if(area[i][j] == 'R')
-          {
-            count++;
-          }
-          if(count>1)
-          {
-            cout<<"Illegal! There are "<<count;
-            cout<<" charging place in the clean area!"<<endl;
-            return 0;
-          }
-        }
-      }
-      if(count ==0)
-      {
-        cout<<"Illegal! There doesn't exist charging place in the clean area"<<endl;
-        return 0;
-      }
-
-      else return 1;
-}
     void set_area_from_input()
     {
       int ver;
       int hor;
       int life_span;
+      int enough_battery = 0;
+      int count =0;
       input>>ver;
       input>>hor;
       input>>life_span;
       //output<<ver_vertex<<" "<<hor_vertex<<" "<<life_span<<endl;
       set_ver(ver);
       set_hor(hor);
+      set_battery(life_span);
       fflush(stdin);
       //getchar();
 
-      int ch;
       int i, j, k;
       //cout<<"bang"<<endl;
-      area = (char**)new char[(ver_vertex)*sizeof(char*) + (hor_vertex)*hor_vertex*sizeof(char)];
+      area = (point**)new point[(ver_vertex)*sizeof(point*) + (hor_vertex)*hor_vertex*sizeof(point)];
       //cout<<endl;
       //cout<<"bang"<<endl;
       for(i = 0; i < (ver_vertex); i++)
       {
-        area[i] = ((char *)(area + ver_vertex) + i*hor_vertex*sizeof(char));
+        area[i] = ((point *)(area + ver_vertex) + i*hor_vertex*sizeof(point));
       }
       cout<<"Now, we have "<<ver_vertex<<"*"<<hor_vertex<<" clean_area."<<endl;
       for(j= 0; j < ver_vertex; j++)
         {
           for(k = 0; k < hor_vertex; k++)
           {
-            area[j][k] = 0;
+            area[j][k].set_shape('0');
           }
         }
+
       for(i=0; i<ver_vertex; i++)
       {
         for(j=0; j<hor_vertex; j++)
@@ -145,18 +183,40 @@ public:
           //cout<<ch<<endl;
           if(ch == '0')
           {
-            area[i][j] = ch;
-            cout<<"area["<<i<<"]["<<j<<"] = "<<area[i][j]<<endl;
+            area[i][j].set_shape(ch);
+            cout<<"area["<<i<<"]["<<j<<"] = "<<area[i][j].get_shape()<<endl;
           }
           else if(ch == '1')
           {
-            area[i][j] = ch;
-            cout<<"area["<<i<<"]["<<j<<"]"<<" = "<<area[i][j]<<endl;
+            area[i][j].set_shape(ch);
+            cout<<"area["<<i<<"]["<<j<<"]"<<" = "<<area[i][j].get_shape()<<endl;
           }
           else if(ch =='R')
           {
-            area[i][j] = ch;
-            cout<<"area["<<i<<"]["<<j<<"]"<<" = R"<<endl;
+            if(count == 0)
+            {
+              area[i][j].set_shape(ch);
+              set_R_x(j);
+              set_R_y(i);
+              cout<<"area["<<i<<"]["<<j<<"]"<<" = "<<area[i][j].get_shape()<<endl;
+              cout<<"The charging place (R) in this area is located at";
+              cout<<"("<<R_x<<", "<<R_y<<")"<<endl;
+              count++;
+            }
+            else
+            {
+              if(count>1)
+              {
+                cout<<"Illegal! There are "<<count;
+                cout<<" charging place in the clean area!"<<endl;
+                exit(1);
+              }
+              else
+              {
+                cout<<"Illegal! There doesn't exist charging place in the clean area"<<endl;
+                exit(1);
+              }
+            }
           }
           else
           {
@@ -168,12 +228,53 @@ public:
       cout<<"Area build complete!"<<endl;
       cout<<"Display clean area as follows: "<<endl;
       print_clean_area();
-      if(!edge_detection()||!charging_place_detection())
+      if(!edge_detection())
       {
         cout<<"illegal clean_area!"<<endl;
+        exit(1);
+      }
+
+      for(int i=0; i<ver_vertex; i++)
+      {
+        for(int j=0; j<hor_vertex; j++)
+        {
+          area[i][j].set_Y(i);
+          area[i][j].set_X(j);
+        }
+      }
+
+      for(int i=0; i<ver_vertex; i++)
+      {
+        for(int j=0; j<hor_vertex; j++)
+        {
+          if(area[i][j].get_shape() == '1')
+          {
+            area[i][j].set_D_to_R(-1);
+          }
+          else
+          {
+            area[i][j].set_D_to_R(area[i][j].Distance_counting(R_x, R_y));
+            if(enough_battery< area[i][j].get_D_to_R())
+            {
+              cout<<"The distance to R from area["<<i<<"]["<<j<<"]"<<" = ";
+              cout<<area[i][j].get_D_to_R()<<endl;
+              enough_battery = area[i][j].get_D_to_R();
+            }
+
+          }
+        }
+      }
+      if(get_battery()<2*enough_battery)
+      {
+        cout<<"The maximum battery is "<<get_battery()<<", which is ";
+        cout<<"less than the doubling of the farthest distance("<<enough_battery<<") in the clean area.";
+        cout<<endl;
+        print_D_to_R();
+        cout<<"illegal clean area! Without enought battery."<<endl;
+        exit(1);
       }
 }
-    char** get_area()
+    point** get_area()
     {
       return area;
     }
@@ -185,36 +286,34 @@ public:
     {
       R_y = y;
     }
-    void R_finding()
+    void print_D_to_R()
     {
-      for(int j= 0; j < ver_vertex; j++)
+      cout<<"The shortest distance from R to all points in the clean area is: "<<endl;
+      for(int i=0; i<ver_vertex; i++)
+      {
+        for(int j=0; j<hor_vertex; j++)
         {
-          for(int k = 0; k < hor_vertex; k++)
+          if(area[i][j].get_D_to_R() == 0)
           {
-            if(area[j][k] == 'R')
-            {
-              set_R_x(j);
-              set_R_y(k);
-              break;
-            }
+            cout<<"R\t";
+          }
+          else if(area[i][j].get_shape() == '0')
+          {
+            cout<<area[i][j].get_D_to_R()<<"\t";
+          }
+          else
+          {
+            cout<<"X\t";
           }
         }
-    }
-    void get_R()
-    {
-      cout<<"The charging place (R) in this area is located at";
-      cout<<"("<<R_x<<", "<<R_y<<")"<<endl;
-    }
-    void clean_area_initializing()
-    {
-      set_area_from_input();
-      R_finding();
-      get_R();
+        cout<<endl<<endl;
+      }
     }
 };
 int main()
 {
   clean_area eg;
-  eg.clean_area_initializing();
+  eg.set_area_from_input();
+  eg.print_D_to_R();
   return 0;
 }
